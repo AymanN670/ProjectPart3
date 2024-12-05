91f3d8360cc203d41b43e08bcf16adef54ed33a3
AymanN670 <ayman.najmuddin@ontariotechu.net>
2024-12-05 15:18:48 -0500

    created images in assets & copied and pasted our codes from our server side for the html into the client side

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutusComponent } from './Pages/aboutus/aboutus.component';
+import { ProductComponent } from './Pages/product/product.component';
+import { ServiceComponent } from './Pages/service/service.component';
+import { ContactusComponent } from './Pages/contactus/contactus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'aboutus', component: AboutusComponent},
+    {path: 'product', component: ProductComponent},
+    {path: 'service', component: ServiceComponent},
+    {path: 'contactus', component: ContactusComponent}
+];
