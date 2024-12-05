04bd671550449e103f44dab61df211d44a4e83fb
AymanN670 <ayman.najmuddin@ontariotechu.net>
2024-12-05 15:21:44 -0500

    updated app.routes.ts file

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,4 +5,10 @@ import { ProductComponent } from './Pages/product/product.component';
 import { ServiceComponent } from './Pages/service/service.component';
 import { ContactusComponent } from './Pages/contactus/contactus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'aboutus', component: AboutusComponent},
+    {path: 'product', component: ProductComponent},
+    {path: 'service', component: ServiceComponent},
+    {path: 'contactus', component: ContactusComponent}
+];
