f443a10d0d3513f0d77787295dc59c34459fe76e
Fatima-sohail123 <fatima.sohail@ontariotechu.net>
2024-12-05 14:45:12 -0500

    made aymans changes on mine

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Partials/header/header.component';
+import { FooterComponent } from './Partials/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
